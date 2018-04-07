b69953c9c00496ed145c2570b4bda07be3922199
AndosonHuang <huangchao6312027@gmail.com>
2018-04-07 15:42:29 -0400

    fix some bugs

--- src/CustomerOperation.java
+++ src/CustomerOperation.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class CustomerOperation {
-	public static void enterNew() {
+	public static void enterCustomer() {
 		String input;
         Scanner sc = new Scanner(System.in);
 
