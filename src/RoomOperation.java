86639ff586c5e16db427d412837319462d9730cf
hlin14 <hlin14@ncsu.edu>
2018-04-10 15:32:34 -0400

    update checkRoomIsAvailableWithRoomCategory

--- src/RoomOperation.java
+++ src/RoomOperation.java
@@ -252,12 +252,13 @@ public class RoomOperation {
                 System.out.println("availability: " + availability);
             }
         } catch (SQLException e) {
-            System.out.println("Checked failed.");
+            System.out.println("Room is not existed. Checked failed.");
         }
     }
     public static void checkRoomIsAvailableWithRoomCategory() {
         String input;
         String pattern = "[0-9]+";
+        String patternForCategory = "[1-3]+";
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -269,11 +270,26 @@ public class RoomOperation {
         int hotelID = Integer.valueOf(input);
 
         while (true) {
-            System.out.print("Room category: ");
+            System.out.println("Room Category: ");
+            System.out.println("1. Single ");
+            System.out.println("2. Deluxe ");
+            System.out.println("3. Presidential ");
             input = sc.nextLine();
-            if (!input.trim().equals("")) break;
+            if (Pattern.matches(patternForCategory, input)) break;
+            else System.out.println("Your input is illegal");
         }
-        String roomCategory = input;
+        String roomCategory = "";
+        switch (input) {
+            case "1":
+                roomCategory = "Single";
+                break;
+            case "2":
+                roomCategory = "Deluxe";
+                break;
+            case "3":
+                roomCategory = "Presidential";
+                break;
+        }
 
         String sql = "SELECT hotel_ID, room_number, availability FROM room WHERE hotel_ID = ? AND room_category = ?";
         Connection conn = DBconnection.getConnection();
@@ -292,7 +308,7 @@ public class RoomOperation {
                 System.out.println("availability: " + availability);
             }
         } catch (SQLException e) {
-            System.out.println("Checked failed.");
+            System.out.println("The ID of hotel is not existed. Checked failed.");
         }
     }
     public static void releaseRoom() {
