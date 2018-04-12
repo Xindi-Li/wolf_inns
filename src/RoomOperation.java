c9ab2eed13027ff6cddf6c277809ee0af6e90e6d
hlin14 <hlin14@ncsu.edu>
2018-04-12 15:23:52 -0400

    try add days

--- src/RoomOperation.java
+++ src/RoomOperation.java
@@ -453,7 +453,7 @@ public class RoomOperation {
 
         Calendar c = Calendar.getInstance();
         c.setTime(new Date()); // Now use today date.
-        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
         while (true) {
 
             System.out.print("Check-out date(yyyy-mm-dd): ");
