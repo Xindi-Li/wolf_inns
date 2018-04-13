59ef003659da61798d14e9502b5bf295292ad451
AndosonHuang <huangchao6312027@gmail.com>
2018-04-13 18:29:09 -0400

    fix some bugs

--- src/ReportOperation.java
+++ src/ReportOperation.java
@@ -30,7 +30,7 @@ public class ReportOperation{
 						System.out.println("Please input hotel ID:");
 						input = sc.next();
 						if (Pattern.matches(pattern, input)){
-							return getNumberOfRoom(Integer.valueOf(input));
+							System.out.println(getNumberOfRoom(Integer.valueOf(input)));
 							break;
 						}
 						else System.out.println("Your input is illegal");
@@ -41,7 +41,7 @@ public class ReportOperation{
 						printRoomType();
 						input = sc.next();
 						if (Pattern.matches(patternForCategory, input)){
-							return getNumberOfRoom(getCategory(Integer.valueOf(input)));
+							System.out.println(getNumberOfRoom(getCategory(Integer.valueOf(input))));
 							break;
 						}
 						else System.out.println("Your input is illegal");
@@ -55,7 +55,7 @@ public class ReportOperation{
 				    		System.out.println("Please input end date:");
 				    		input = sc.next();
 				    		if (Pattern.matches(patternForDate, input)){
-				    			return getNumberOfRoom(start_date, input);
+				    			System.out.println(getNumberOfRoom(start_date, input));
 				    			break;
 				    		}
 				    		else{
@@ -71,7 +71,11 @@ public class ReportOperation{
 		}
 	}
 
-	private void printRoomType(){
+	public static void getNumberOfAvailableRoom(){
+		return;
+	}
+
+	private static void printRoomType(){
 		System.out.println("=====================================");
         System.out.println("1. Single");
         System.out.println("2. Deluxe");
@@ -79,7 +83,7 @@ public class ReportOperation{
         System.out.println("====================================="); 
 	}
 
-	private String getCategory(int i) {
+	private static String getCategory(int i) {
 		switch (i) { 
 			case 1:
 			    return "Single";
@@ -102,23 +106,10 @@ public class ReportOperation{
 
 
 	public static int getNumberOfAvailableRoom(int id){
-		String input;
-        String pattern = "[0-9]+";// for int pattern
-        String patternForDecimal = "[0-9]+.[0-9]+";
-        String patternForBoolean = "[0-1]+";
-        String patternForCategory = "[1-3]+";
-        Scanner sc = new Scanner(System.in);
-
-        while(true) {
-        	System.out.println("Hotel ID: ");
-        }
-	}
-
-	public static int getNumberOfRoom(int id){
 		return 1;
 	}
 
-	public static int getNumberOfAvailableRoom(int id){
+	public static int getNumberOfRoom(int id){
 		return 1;
 	}
 
