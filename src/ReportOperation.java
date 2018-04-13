2506f3bac82357a83ec8820cf40669045b665f44
AndosonHuang <huangchao6312027@gmail.com>
2018-04-13 19:56:56 -0400

    add available ways

--- src/ReportOperation.java
+++ src/ReportOperation.java
@@ -84,7 +84,67 @@ public class ReportOperation{
 	}
 
 	public static void getNumberOfAvailableRoom(){
-		return;
+		Scanner sc = new Scanner(System.in);
+		String input;
+		String choice;
+		String pattern = "[0-9]+";
+		String patternForCategory = "[1-3]+";
+		String patternForDate = "\\d{4}-\\d{2}-\\d{2}";
+
+		while (true) {	
+			printRoomMethodMenu();
+			System.out.print("Operation No:");
+
+			input = sc.next();
+			switch (input) {
+				case "1":
+					System.out.print("Please input hotel ID:");
+					choice = sc.next();
+					if (Pattern.matches(pattern, choice)){
+						System.out.println(getNumberOfAvailableRoomByHotelID(choice));
+						break;
+					}
+					else {
+						System.out.println("Your input is illegal");
+						break;
+					}				
+				case "2":		
+					printRoomType();
+					System.out.print("Please input room type:");			
+					choice = sc.next();
+					if (Pattern.matches(patternForCategory, choice)){
+						System.out.println(getNumberOfAvailableRoomByCategory(getCategory(Integer.valueOf(choice))));
+						break;
+					}
+					else {
+						System.out.println("Your input is illegal");
+						break;
+					}		
+				case "3":		    
+			    	System.out.print("Please input start date:");
+			    	choice = sc.next();
+			    	if (Pattern.matches(patternForDate, choice)){
+			    		String start_date = choice;
+			    		System.out.println("Please input end date:");
+			    		choice = sc.next();
+			    		if (Pattern.matches(patternForDate, choice)){
+			    			System.out.println(getNumberOfAvailableRoomByPeriod(start_date, choice));
+			    			break;
+			    		}
+			    		else{
+			    			System.out.println("Your input is illegal.");
+			    			break;
+			    		}
+			    	}else {
+			    		System.out.println("Your input is illegal.");
+			    		break;
+			    	}
+				    
+				default:
+				    System.out.println("Your input is illegal.");
+				    break;
+			}
+		}
 	}
 
 	private static void printRoomType(){
@@ -138,10 +198,41 @@ public class ReportOperation{
 	}
 
 	public static int getNumberOfAvailableRoomByHotelID(String id){
-		return 1;
+		String sql = "select count(*) as total from room where hotel_ID = ? and availability = 1";
+		Connection conn = DBconnection.getConnection();
+		try {
+			// System.out.println("int try segment");
+            PreparedStatement ptmt = conn.prepareStatement(sql);
+            ptmt.setString(1, id);
+            ResultSet rs = ptmt.executeQuery();
+            // System.out.println("get ResultSet");
+            while(rs.next()){
+	            // System.out.println(rs.getInt("total"));
+	            return rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+		return -1;
 	}
 
 	public static int getNumberOfAvailableRoomByCategory(String str){
+		String sql = "select count(*) as total from room where room_category = ? and availability = 1";
+		Connection conn = DBconnection.getConnection();
+		try {
+			// System.out.println("int try segment");
+            PreparedStatement ptmt = conn.prepareStatement(sql);
+            ptmt.setString(1, str);
+            ResultSet rs = ptmt.executeQuery();
+            // System.out.println("get ResultSet");
+            while(rs.next()){
+	            // System.out.println(rs.getInt("total"));
+	            return rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+		return -1;
 		return 2;
 	}
 
